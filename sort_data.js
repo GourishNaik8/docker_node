2e617abd6b0ff580b0445d5f21bd21801b18e5cf
Gourishankar-Naik <53259414+GourishNaik8@users.noreply.github.com>
2023-09-22 14:17:15 +0000

    refined code

--- sort_data.js
+++ sort_data.js
@@ -28,20 +28,17 @@ function sortBy(data,option) {
         case 'bool':
               // sort based on boolean value
               return data.sort((a, b) => b.available - a.available);
-              break;
        case 'str':
               // sort based on the string compare
               return data.sort((a,b) => a.str === 'true' ? -1 : b.str === 'true'? 1 : 0)
-              break;
        case 'random':
               // shuffle data randomly
               return data.sort(() => Math.random() - .3) // .3 is the value the differs/ randomizes the data
-              break;
         // if not option is available then default data is returned.
         default:
               return data
   }
 }
 
-const res = sortBy(data) // you can pass the option as a string in the function.
+const res = sortBy(data,'') // you can pass the option as a string in the function.
 console.log(res);
