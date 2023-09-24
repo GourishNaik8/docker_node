3bdde88f6ececa09c2e0b32e7e2179dfa6f3c6a7
Gourishankar-Naik <53259414+GourishNaik8@users.noreply.github.com>
2023-09-24 10:00:51 +0000

    added the stack dsa tried the export the class as modules and import them in another js file but getting some errors in the environment

---
+++ DSA/Stack/Stack.js
@@ -0,0 +1,46 @@
+//constructor function for stack with methos push, pop, top, and isEmpty in es5 */
+export function Stack_es5() {
+  this.items = [];
+}
+
+Stack_es5.prototype.push = function(item) {
+  this.items.push(item);
+};
+
+Stack_es5.prototype.pop = function() {
+  return this.items.pop();
+};
+
+Stack_es5.prototype.top = function() {
+  return this.items[this.items.length - 1];
+};
+
+Stack_es5.prototype.isEmpty = function() {
+  return this.items.length === 0;
+}; 
+
+// function stack in es6 format
+
+export class Stack_es6 {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+//export stack class as module to use in another file
